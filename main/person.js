a54e3a5bec9a454fadc900a0b337df3096c98376
lxxhack <1572411660@qq.com>
2017-06-12 23:23:48 +0800

    Create person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,11 @@
 // Write your code here
-module.exports = Person
+module.exports = Person;
+function Person(name,age)
+{
+  this.name=name;
+  this.age=age;
+  this.introduce=function()
+  {
+    conlog("My name is ",name,". I am ",age," years old.");
+  }
+}
