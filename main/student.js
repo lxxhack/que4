292f2e3443a6d1c80a12b4e22af9b0c52f319fba
lxxhack <1572411660@qq.com>
2017-06-12 23:36:41 +0800

    Create student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,12 @@
-let Person = require('./person')
+let Person = require('./person');
 // Write your code here
-module.exports = Student
+module.exports = Student;
+function Student(name,age,klass)
+{
+  Person(name,age);
+  this.klass=klass;
+  this.introduce=function()
+  {
+    return "My name is "+name+". I am "+age+" years old. I am a Student. I am at Class "+klass+".";
+  }
+}
